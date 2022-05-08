0b65a8b55fe6beaebb96ea036534096d8ff6a2d2
Alfredo Vazquez <jose.vazquez@etalons.com.mx>
2022-05-07 20:27:26 -0500

    Creating views and components

---
+++ src/app/components/item-user/item-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-user',
+  templateUrl: './item-user.component.html',
+  styleUrls: ['./item-user.component.scss'],
+})
+export class ItemUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
